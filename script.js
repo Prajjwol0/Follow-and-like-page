2c856b14e79ed90f50b46bd92fab7d94255c0681
Mr. Basnet <basnetssahil@gmail.com>
2025-02-11 20:23:17 +0545

    init

--- script.js
+++ script.js
@@ -1,101 +1,84 @@
-// var text = document.getElementById(detail);
+let btnEl = document.getElementById("btn")
+let likeEl = document.querySelector(".fa-heart")
+let saveEl = document.querySelector(".fa-bookmark")
+let shareEl = document.querySelector(".fa-paper-plane")
 
-// detail:following or not
-var text = document.querySelector("#detail");
+let followingEl = document.getElementById("followingId")
+let popupImageEl = document.getElementById("transparentHeart")
 
-// like icon
-var like = document.querySelector("#heart1");
-// console.log(detail)
+let likeIndex = 0, followIndex = 0, saveIndex = 0, shareIndex = 0
 
-// follow button
-var button = document.querySelector("#button");
+function likePopup(){
+    popupImageEl.style.display = "flex"
 
-// popping heart 
-var heartpop = document.querySelector("#heartpop");
-
-// ronaldo image
-var ronaldo = document.querySelector("#ronaldo");
-
-// changes button style on follow and unfollow
-var a = 1;
-button.addEventListener("click", function () {
-    if (a == 1) {
-        text.innerHTML = "Following";
-        text.style.color = "green";
-        button.innerHTML = "Unfollow";
-        button.style.color = "black";
-        button.style.backgroundColor = "white";
-        a = 2;
-    }
-    else {
-        button.innerHTML = "Follow";
-        text.innerHTML = "Not following yet";
-        text.style.color = "rgb(204, 2, 2)";
-        button.style.color = "rgb(255, 255, 255)";
-        button.style.backgroundColor = "#0866ff";
-        a = 1;
-    }
-})
-
-
-// abc=0;
-// var c=1;
-// ronaldo.addEventListener("dblclick",function(){
-//     if (c==1){
-//     heartpop.style.visibility = ("visible");
-// c=2;
-//     }
-//     else {
-//     heartpop.style.visibility = ("hidden");
-// c=1;
-//     }
-    
-// setTimeout(function(){
-//     if(c==2)
-//     {
-//      heartpop.style.visibility = ("hidden");
-//      c=1;
-//     }
-//  },1500)
- 
-//     // console.log(abc=abc+1)
-// })
-
-
-
-// var idk =
-
-// setTimeout helps to execute code after given time period
-// to again hide the heartpop after liking the photo using setTimeout 
-function pop(){
-    heartpop.style.visibility = ("visible");
-    if (like.src.includes("transparentHeart.png")) {
-        like.src = "redHeart.png";}
-    setTimeout(function(){
-    heartpop.style.visibility = ("hidden");
-
+    setTimeout(()=>{
+        popupImageEl.style.display = "none"
     },1000)
-
 }
-ronaldo.addEventListener("dblclick",pop)
 
-// to pop the heart even when the like icon is clicked
-// pop function is called inside like function
+btnEl.addEventListener("click",()=>{
 
-// changes heart style on like or unlike
-var b = 1;
-like.addEventListener("click", function () {
-    // b=b+1;
-    // console.log(b)
-    if (like.src.includes("transparentHeart.png")) {
-        like.src = "redHeart.png";
-        pop()
-    } else {
-        like.src = "transparentHeart.png";
-        // heartpop.style.display = ("none");
+    if(followIndex%2==0){
+        btnEl.innerText = "Unfollow"
+        followingEl.innerText = "Following."
+
+      
 
     }
+    else{
+        btnEl.innerText = "Follow"
+        followingEl.innerText = "Not Following Yet."
+    }
+   followIndex++
+})
+
+
+likeEl.addEventListener("click",()=>{
+ 
+    if(likeIndex%2==0){
+        likeEl.classList.remove("fa-regular")
+        likeEl.classList.add("fa-solid")
+     
+        likePopup()
+        
+    }
+    else{
+        likeEl.classList.remove("fa-solid")
+        likeEl.classList.add("fa-regular")
+    }
+    likeIndex++
+})
+
+saveEl.addEventListener("click",()=>{
+   
+     
+    if(saveIndex%2==0){
+        saveEl.classList.remove("fa-regular")
+        saveEl.classList.add("fa-solid")
+
+        
+    }
+    else{
+        saveEl.classList.remove("fa-solid")
+        saveEl.classList.add("fa-regular")
+    }
+    saveIndex++
 
 })
-// const date = new Date();
-// console.log(date)
+
+shareEl.addEventListener("click",()=>{
+   
+     
+    if(shareIndex%2==0){
+        shareEl.classList.remove("fa-regular")
+        shareEl.classList.add("fa-solid")
+
+        
+    }
+    else{
+        shareEl.classList.remove("fa-solid")
+        shareEl.classList.add("fa-regular")
+    }
+    shareIndex++
+
+})
